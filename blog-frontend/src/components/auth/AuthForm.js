297ae3db2b5061b2f3336a479e429ffb0e99e4aa
overaction <kmc04@naver.com>
2020-04-22 14:37:16 +0000

    UI준비하기-1

---
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/*회원가입 또는 로그인 폼*/
+
+const AuthformBlock = styled.div``;
+
+const AuthForm = () => {
+    return (
+        <AuthformBlock>
+            AuthForm
+        </AuthformBlock>
+    )
+}
+
+export default AuthForm;
